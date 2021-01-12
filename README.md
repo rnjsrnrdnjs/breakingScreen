78d44ebab16b06c722914d9439e097e193865136
rnjsrnrdnjs <090tree@gmail.com>
2021-01-12 14:40:07 +0000

    two

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # breakingScreen
 <p>캡쳐화면 부수기 프로젝트</p>
+<p>https://pixabay.com/ko/    이미지 제공주소 </p>
+<p> 시작 방법 phonegap serve </p>
 
--- breakingScreen/www/js/winOnload.js
+++ breakingScreen/www/js/winOnload.js
@@ -1,3 +1,39 @@
+var bg;
+var cvs,ctx;
+var main;
 window.onload=()=>{
+	main=document.getElementById('main');
 	
+	var sel=document.getElementById('select');
+	bg=new Image();
+	sel.addEventListener('change',(e)=>{
+		if(sel.files.length==0)return;
+		var reader=new FileReader();
+		reader.onload=function(){
+			bg.src=reader.result;
+		}
+		reader.readAsDataURL(sel.files[0]);
+		cvs=elt("canvas",{id:"canvas"});
+		ctx=cvs.getContext('2d');	
+		document.body.removeChild(main);
+		document.body.appendChild(cvs);
+		draw();
+	},false);
+	draw();
+	
+};
+
+
+/*
+function play(){
+	다시할때 처음화면 돌아가기 
+	document.body.append(main);
+	document.body.removeChild(cvs);
+}
+*/
+
+function draw(){
+	ctx.drawImage(bg,0,0);
+	
+	requestAnimationFrame(draw);
 };
