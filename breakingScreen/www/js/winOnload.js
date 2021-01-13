e057b3e32d1a549c0a900598a3b148cb35ba5cd8
rnjsrnrdnjs <090tree@gmail.com>
2021-01-13 14:55:32 +0000

    three

--- breakingScreen/www/js/winOnload.js
+++ breakingScreen/www/js/winOnload.js
@@ -1,25 +1,45 @@
 var bg;
 var cvs,ctx;
-var main;
+var main,cmain; var w,h;
 window.onload=()=>{
 	main=document.getElementById('main');
-	
+	cmain=main.cloneNode();
+	//main.style.backgroundImage = "url('../img/mainm.jpg')";
 	var sel=document.getElementById('select');
+	var con=document.getElementById('control');
+	w=main.offsetWidth;
+	h=main.offsetHeight;
+	
+	
 	bg=new Image();
 	sel.addEventListener('change',(e)=>{
 		if(sel.files.length==0)return;
+		cvs=elt("canvas",{id:"canvas"});
+		ctx=cvs.getContext('2d');	
+		
 		var reader=new FileReader();
 		reader.onload=function(){
 			bg.src=reader.result;
 		}
+		bg.onload=()=>{
+			bg.width=w;
+			bg.height=h;
+		}
 		reader.readAsDataURL(sel.files[0]);
-		cvs=elt("canvas",{id:"canvas"});
-		ctx=cvs.getContext('2d');	
+
+		console.log(main.offsetWidth+" "+main.offsetHeight);
+		console.log(cmain.offsetWidth+" "+cmain.offsetHeight);
+
+
 		document.body.removeChild(main);
 		document.body.appendChild(cvs);
+
+		console.log(main.offsetWidth+" "+main.offsetHeight);
+		console.log(cmain.offsetWidth+" "+cmain.offsetHeight);
+	
+	
 		draw();
 	},false);
-	draw();
 	
 };
 
@@ -33,7 +53,7 @@ function play(){
 */
 
 function draw(){
-	ctx.drawImage(bg,0,0);
-	
+	ctx.drawImage(bg,0,0,w,h);
+		
 	requestAnimationFrame(draw);
 };
